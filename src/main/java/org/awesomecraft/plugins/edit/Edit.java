f0415fcad3d236910433485b50ed828ec5583c69
Will Wright <willjwright@mac.com>
2012-01-01 15:57:09 -0800

    Added commands in plugin.yml

--- src/main/java/org/awesomecraft/plugins/edit/Edit.java
+++ src/main/java/org/awesomecraft/plugins/edit/Edit.java
@@ -4,13 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Edit extends JavaPlugin {
     public void onDisable() {
-        // TODO: Place any custom disable code here.
         System.out.println(this + " is now disabled!");
     }
 
     public void onEnable() {
-        // TODO: Place any custom enable code here, such as registering events
-        
         System.out.println(this + " is now enabled!");
     }
 }
