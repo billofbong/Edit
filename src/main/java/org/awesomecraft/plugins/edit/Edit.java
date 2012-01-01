40c5be55385cfca15d309b63e24d839a59afdc79
Will Wright <willjwright@mac.com>
2012-01-01 15:51:55 -0800

    First Commit

---
+++ src/main/java/org/awesomecraft/plugins/edit/Edit.java
@@ -0,0 +1,16 @@
+package org.awesomecraft.plugins.edit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Edit extends JavaPlugin {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+        System.out.println(this + " is now disabled!");
+    }
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here, such as registering events
+        
+        System.out.println(this + " is now enabled!");
+    }
+}
