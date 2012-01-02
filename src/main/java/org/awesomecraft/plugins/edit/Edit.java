e68652292250c837c5b21f42107df7abce6e843c
Will Wright <willjwright@mac.com>
2012-01-01 16:20:34 -0800

    Added /em command

--- src/main/java/org/awesomecraft/plugins/edit/Edit.java
+++ src/main/java/org/awesomecraft/plugins/edit/Edit.java
@@ -1,5 +1,8 @@
 package org.awesomecraft.plugins.edit;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.ChatColor;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,7 +11,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Edit extends JavaPlugin {
-    int bId = 0;
+    Map<String, Integer> bId = new HashMap<String, Integer>();
     public void onDisable() {
         System.out.println(this + " is now disabled!");
     }
@@ -17,10 +20,29 @@ public class Edit extends JavaPlugin {
         getCommand("e").setExecutor(new CommandExecutor() {
 
             public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
-                if(args.length == 1) {
+               if(cs instanceof Player) { 
+                if(args.length == 0) {
                     Player sender = (Player) cs;
                     Block btar = sender.getTargetBlock(null, 120);
-                    btar.setTypeId(bId);
+                    btar.setTypeId(bId.get(sender.toString()));
+                    return true;
+                }
+               }
+                return false;
+            }
+            
+        });
+        getCommand("em").setExecutor(new CommandExecutor() {
+
+            public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
+                if(cs instanceof Player) {
+                    if(args.length == 1) {
+                        Integer int1 = Integer.parseInt(args[0]);
+                        Player sender = (Player) cs;
+                        bId.put(sender.toString(), int1);
+                        sender.sendMessage(ChatColor.GREEN + "Set edit material to " + int1 + ".");
+                        return true;
+                    }
                 }
                 return false;
             }
