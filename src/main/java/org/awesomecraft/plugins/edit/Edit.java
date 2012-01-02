163907624b65c90ed93bdac83b44b1486e9a8a72
Will Wright <willjwright@mac.com>
2012-01-01 16:06:08 -0800

    Added /e command

--- src/main/java/org/awesomecraft/plugins/edit/Edit.java
+++ src/main/java/org/awesomecraft/plugins/edit/Edit.java
@@ -1,13 +1,30 @@
 package org.awesomecraft.plugins.edit;
 
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Edit extends JavaPlugin {
+    int bId = 0;
     public void onDisable() {
         System.out.println(this + " is now disabled!");
     }
 
     public void onEnable() {
+        getCommand("e").setExecutor(new CommandExecutor() {
+
+            public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
+                if(args.length == 1) {
+                    Player sender = (Player) cs;
+                    Block btar = sender.getTargetBlock(null, 120);
+                    btar.setTypeId(bId);
+                }
+                return false;
+            }
+        });
         System.out.println(this + " is now enabled!");
     }
 }
